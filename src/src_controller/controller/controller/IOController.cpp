6e7822c2139f9bc10ea8408f2bb1682ff40d21c2
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2020-11-01 16:03:49 +0100

    working io controller

--- src/src_controller/controller/controller/IOController.cpp
+++ src/src_controller/controller/controller/IOController.cpp
@@ -6,10 +6,129 @@ IOController::IOController()
 	
 	if (!SPICommunication::getInstance()->isInitialised()) {
 		if (SPICommunication::getInstance()->lastErrorCode() != 0) {
+			board_init_success = false;
 			return;
+			
 		}
+		board_init_success = false;
+		return;
 	}
 	
 	SPI_CS_DEVICE = SPICommunication::SPI_DEVICE::IO_CONTROLLER;
 	SPICommunication::getInstance()->register_cs_gpio(SPI_CS_DEVICE, SPI_CS_GPIO);
+
+	//CHECKS THE STATE OF THE BOARD
+	uint8_t buffer[] = {IOController::COMMANDS::STATUS_LED};
+	int res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer, 1);
+	res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer, 1);
+	//CHECK THE RESULT
+	if(res == 0 && buffer[0] == IOController::COMMANDS::STATUS_LED) {
+		board_init_success = true;
+	}
+}
+
+void IOController::setCoilState(IOController::COIL _coil, bool _state)
+{
+	
+	unsigned char cmd = 0;
+	if (_coil == IOController::COIL::COIL_A)
+	{
+		if (_state)
+		{
+			cmd = IOController::COMMANDS::SET_COIL_A;
+		}
+		else
+		{
+			cmd = IOController::COMMANDS::DISABLE_COIL_A;
+		}
+	}
+	else
+	{
+		if (_state)
+		{
+			cmd = IOController::COMMANDS::SET_COIL_B;
+		}
+		else
+		{
+			cmd = IOController::COMMANDS::DISABLE_COIL_B;
+		}
+	}
+	
+	uint8_t buffer_w[] = {cmd};
+	uint8_t buffer_r[] = { 0 };
+	
+	int res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_w, 1); //WRITE CMD
+	res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_r, 1);  //READ BACK
+}
+
+void IOController::setStatusLed(IOController::LED _led, bool _state)
+{
+	unsigned char cmd = 0;
+	if (_led == IOController::LED::STAUS_LED_A)
+	{
+		if (_state)
+		{
+			cmd = IOController::COMMANDS::SET_STATUS_LED;
+		}
+		else
+		{
+			cmd = IOController::COMMANDS::DISABLE_STATUS_LED;
+		}
+	}
+	else
+	{
+		if (_state)
+		{
+			cmd = IOController::COMMANDS::SET_STATUS_LED;
+		}
+		else
+		{
+			cmd = IOController::COMMANDS::DISABLE_STATUS_LED;
+		}
+	}
+	
+	uint8_t buffer_w[] = { cmd };
+	uint8_t buffer_r[] = { 0 };
+	
+	int res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_w, 1);  //WRITE CMD
+	res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_r, 1);   //READ BACK
+}
+
+ChessPiece::FIGURE IOController::ScanNFC(int _retry_count)
+{
+	//POPULATE CMD ARRAYS
+	uint8_t buffer_w[] = { IOController::COMMANDS::NFC_READ_RQUEST };
+	uint8_t buffer_r[] = { 0 };
+	
+	//SEND NFC READ REQUEST
+	int res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_w, 1);   //WRITE CMD
+	res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_r, 1);    //READ BACK
+	//WAIT FOR ANSWER
+	volatile int retry = 0;
+	while(buffer_r[0] == 0 || !(retry < _retry_count))
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(NFC_READ_ACK_DELAY));
+		res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_r, 1);
+		retry++;
+	}
+	//SEND NFC READ/FINISH COMMAND
+	buffer_w[0] = IOController::COMMANDS::NFC_READ_CANCEL;
+	res = SPICommunication::getInstance()->spi_write(SPI_CS_DEVICE, buffer_w, 1);
+	
+	//PARSE RESULT TO A FIGURE
+	ChessPiece::FIGURE p =ChessPiece::NDEF2Figure(buffer_r[0]);
+	
+	return p;
+}
+
+bool  IOController::isInitialized()
+{
+	return board_init_success;
+}
+
+
+
+IOController::~IOController()
+{
+	
 }
--- src/src_controller/controller/controller/IOController.h
+++ src/src_controller/controller/controller/IOController.h
@@ -4,9 +4,16 @@
 
 
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+
+
 #include "SPICommunication.h"
 #include "ChessPiece.h"
 
+
+#define NFC_READ_ACK_DELAY 5 ///HOW LONG TO WAIT FOR NFC DATA IN MS
 class IOController
 {
 	
@@ -15,27 +22,43 @@ public:
 	
 	enum COIL
 	{
-		COIL_A,
-		COIL_B
+		COIL_A = 0,
+		COIL_B = 1
 	};
 	
-	enum STATUS_LED
+	enum LED
 	{
-		STAUS_LED_A,
-		STATUS_LED_B
+		STAUS_LED_A = 0,
+		STATUS_LED_B = 1
 	};
 	
 	IOController();
 	~IOController();
 	
-	ChessPiece::TYPE ScanNFC(int _retry_count = 1);
+	ChessPiece::FIGURE ScanNFC(int _retry_count = 1);
 	
 	void setCoilState(IOController::COIL _coil, bool _state);
-	void setStatusLed(IOController::STATUS_LED _led, bool _state);
+	void setStatusLed(IOController::LED _led, bool _state);
 	void setCoilStrenght(IOController::COIL _coil, uint8_t _strenght, bool _invert_field);
-	
+	bool isInitialized();
 private:
-	
+	///THIS ENUM CONTAINS ALL POSSIBLE COMMANDS TO THE IO BOARD
+	enum COMMANDS
+	{
+		NONE = 0,
+		SET_STATUS_LED = 1,
+		DISABLE_STATUS_LED = 2,
+		STATUS_LED = 3,
+		RESERVED = 4,
+		SET_COIL_A = 5,
+		DISABLE_COIL_A = 6,
+		SET_COIL_B = 7,
+		DISABLE_COIL_B = 8,
+		NFC_READ_RQUEST = 9,
+		NFC_READ_ANSWER = 10,
+		NFC_READ_CANCEL = 0,
+	};
+	bool board_init_success = false;
 	const int SPI_CS_GPIO = 3; //WIRING PI NUMBERING
 	SPICommunication::SPI_DEVICE SPI_CS_DEVICE;
 };
