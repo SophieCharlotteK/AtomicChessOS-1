78685099b451c4bbbac0a5f64c436296b3b006f4
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2021-01-04 20:32:54 +0100

    .

--- src/src_controller/untitled/SHARED/guicommunicator/compile_protobuffer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-~/Desktop/AtomicChessOS_BUILDROOT/src/src_buildroot/buildroot/output/host/bin/protoc --cpp_out . ./PROTOCOL_MSG.proto
-protoc --cpp_out . ./PROTOCOL_MSG.proto
--- src/src_controller/untitled/SHARED/guicommunicator/documentation/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_5finternal_6',['_Internal',['../classprotocolmsg_1_1gui2backend__msg_1_1___internal.html',1,'protocolmsg::gui2backend_msg']]]
-];
